5ea602a12c7c292aa222397fe5e898bb3dfd07b7
Mitali01-Java <mitali.yadav.java@gmail.com>
2026-07-01 22:13:24 +0530

    First Commit

---
+++ src/main/java/com/mitali/HbProject10AssossciationMappingOneToOneApplication.java
@@ -0,0 +1,13 @@
+package com.mitali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbProject10AssossciationMappingOneToOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HbProject10AssossciationMappingOneToOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mitali/HbProject10AssossciationMappingOneToOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbProject10AssossciationMappingOneToOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
